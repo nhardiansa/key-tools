592d906322a9d796ec8aec18050395f12b4598fe
Nabil Hardiansa <nhardiansa@gmail.com>
2025-11-04 17:06:50 +0800

    feat: update route component to include feedback link

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,11 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/"!</div>
+  return (<div>
+    Go to <Link to="/feedback">Feedback</Link>
+  </div>)
 }
