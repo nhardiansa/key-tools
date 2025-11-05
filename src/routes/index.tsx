c2e90c6c7e0ad5f3e73a591df40ce3ca67a94422
Nabil Hardiansa <44939789+nhardiansa@users.noreply.github.com>
2025-11-05 08:19:49 +0800

    Main (#2)
    * Development (#1)
    
    * feat: enhance feedback form and add reviews route with card components
    
    * feat: add missing rewrite rule for reviews route in Vercel configuration
    
    * fix: remove unused Link import and update RouteComponent message
    
    * style: enhance RouteComponent layout with flexbox and heading

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,14 @@
-import { createFileRoute, Link } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return (<div>
-    Go to <Link to="/feedback">Feedback</Link>
-  </div>)
+  return (
+    <div className='flex flex-col items-center justify-center h-screen gap-4'>
+      <h1 className='font-bold text-3xl'>
+        Hayo lo mau ngapain ke sini?
+      </h1>
+    </div>)
 }
